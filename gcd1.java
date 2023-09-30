68446fcc9b3d563e767fb5447cdfea67016421b8
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2023-10-01 02:46:54 +0530

    Create gcd1.java

---
+++ gcd1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int N1=sc.nextInt();
+		int N2=sc.nextInt();
+		while(N1!=N2){
+            if(N1>N2){
+				N1=N1-N2;
+			}
+			else{
+				N2=N2-N1;
+			}
+		}
+		System.out.println(N2);
+
+    }
+}
